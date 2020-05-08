efd6887230d8123e8045ad8f57dde8115dba9812
mike <michgoldennathan17@gmail.com>
2020-05-08 10:55:29 +0100

    [Update] Update  gemfile, Add validations to houses model

--- app/models/house.rb
+++ app/models/house.rb
@@ -3,4 +3,6 @@
 class House < ApplicationRecord
   has_many :favorites
   has_many :users, -> { distinct }, through: :favorites
+
+  validates :name, :details, :image, :price, :rating, presence: true
 end
