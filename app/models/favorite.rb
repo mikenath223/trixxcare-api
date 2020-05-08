04202846e4ef1579838b6bc07055c84268083ec8
mike <michgoldennathan17@gmail.com>
2020-05-08 17:38:05 +0100

    [Update] Clean file tree to stepdown rails

--- app/models/favorite.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Favorite < ApplicationRecord
-  belongs_to :house
-  belongs_to :user
-
-  validates :user_id, uniqueness: { scope: :house_id, message: 'Apartment already added to favorites!' }
-end
--- app/models/house.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class House < ApplicationRecord
-  has_many :favorites
-  has_many :users, -> { distinct }, through: :favorites
-
-  validates :name, :details, :image, :price, :rating, presence: true
-end
